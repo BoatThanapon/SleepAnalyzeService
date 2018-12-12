cf072a4776fda2c9390a727500214eeabfe5e17c
BoatThanapon <tnc@ii.co.th>
2018-12-13 00:19:03 +0700

    Comment fromdate

--- get_node_data.php
+++ get_node_data.php
@@ -7,7 +7,7 @@
             date_default_timezone_set('Asia/Bangkok');
             $now = date('Y-m-d H:i:s');
             $fromDate = date("Y-m-d H:i:s", strtotime('-30 minutes'));
-            echo $fromDate;
+            // echo $fromDate;
             $sqldata = mysql_query("SELECT * FROM node_data Where user_id= '$userID' and created_at between '$fromDate' and '$now'");
 
             $rows = array();
