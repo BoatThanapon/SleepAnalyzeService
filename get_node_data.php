c0f33cc37757893726070817fb63bc136a1158c2
BoatThanapon <tnc@ii.co.th>
2018-12-12 22:51:43 +0700

    get node data before 30 minutes to now

--- get_node_data.php
+++ get_node_data.php
@@ -4,7 +4,11 @@
     class Nodedata{
 
         public function getNodeData($userID){
-            $sqldata = mysql_query("SELECT * FROM node_data Where user_id= '$userID'");
+            date_default_timezone_set('Asia/Bangkok');
+            $now = date('Y-m-d H:i:s');
+            $fromDate = date("Y-m-d H:i:s", strtotime('-30 minutes'));
+            echo $fromDate;
+            $sqldata = mysql_query("SELECT * FROM node_data Where user_id= '$userID' and created_at between '$fromDate' and '$now'");
 
             $rows = array();
             while($r = mysql_fetch_assoc($sqldata)) {
@@ -12,7 +16,6 @@
             }
             
             return json_encode($rows);
-
         }
 
     }
